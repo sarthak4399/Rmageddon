9464c537974376dc9b82454f67bb9c63bc636956
sarthak <sarthak@sarthaks-MacBook-Air.local>
2023-01-25 12:54:42 +0530

    fixing fotter alignment

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -17,27 +17,6 @@ const Footer = () => (
       </div>
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         <div className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
-          {/* club */}
-          <div>
-            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
-              Club
-            </h4>
-            <ul className="list-none mt-4">
-              <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer `}>
-                <a href="">  About</a>
-              </li>
-            </ul>
-            <ul className="list-none mt-4">
-              <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
-                <a href="">  Privacy </a>
-              </li>
-            </ul>
-            <ul className="list-none mt-4">
-              <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer `}>
-                <a href=""> Terms & Condittion </a>
-              </li>
-            </ul>
-          </div>
           {/* otherlinks */}
           <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
             OtherLinks
@@ -63,8 +42,29 @@ const Footer = () => (
               <a href="">Connect </a>
             </li>
           </ul>
+          
         </div>
-
+        {/* club */}
+        <div>
+          <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+            Club
+          </h4>
+          <ul className="list-none mt-4">
+            <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer `}>
+              <a href="">  About</a>
+            </li>
+          </ul>
+          <ul className="list-none mt-4">
+            <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+              <a href="">  Privacy </a>
+            </li>
+          </ul>
+          <ul className="list-none mt-4">
+            <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer `}>
+              <a href=""> Terms & Condittion </a>
+            </li>
+          </ul>
+        </div>
         {/* contact us  */}
         <div>
           <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white text-left ">
@@ -78,6 +78,7 @@ const Footer = () => (
         </div>
       </div>
     </div>
+
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
         Copyright Ⓒ 2023 RNXG. All Rights Reserved.
