85e348ef51fad07928bdab6807b3ac387cfb5cf1
sarthak <sarthak@sarthaks-MacBook-Air.local>
2023-01-25 12:47:14 +0530

    chnages in card text changes in footer chnages experiecnce font

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
     name="connect"
     className={`${styles.flexCenter} ${styles.paddingY} flex-col`}
   >
-    {/* <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className="flex-[1] flex flex-col justify-start mr-10">
         <h1 className="text-gradient text-3xl text-bold">RNXG</h1>
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
@@ -16,32 +16,68 @@ const Footer = () => (
         </p>
       </div>
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
-        {footerLinks.map((footerlink) => (
-          <div
-            key={footerlink.title}
-            className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}
-          >
+        <div className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
+          {/* club */}
+          <div>
             <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
-              {footerlink.title}
+              Club
             </h4>
             <ul className="list-none mt-4">
-              {footerlink.links.map((link, index) => (
-                <li
-                  key={link.name}
-                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
-                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
-                  }`}
-                >
-                  {link.name}
-                  <a href="http://google.com"></a>
-                </li>
-              ))}
+              <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer `}>
+                <a href="">  About</a>
+              </li>
+            </ul>
+            <ul className="list-none mt-4">
+              <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+                <a href="">  Privacy </a>
+              </li>
+            </ul>
+            <ul className="list-none mt-4">
+              <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer `}>
+                <a href=""> Terms & Condittion </a>
+              </li>
             </ul>
           </div>
-        ))}
-      </div>
-    </div> */}
+          {/* otherlinks */}
+          <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+            OtherLinks
+          </h4>
+          <ul className="list-none mt-4">
+            <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer `}>
+              <a href="">Home </a>
+            </li>
+          </ul>
+          <ul className="list-none mt-4">
+            <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+              <a href="">Treasure </a>
+            </li>
+          </ul>
+          <ul className="list-none mt-4">
 
+            <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer `}>
+              <a href="">Battle </a>
+            </li>
+          </ul>
+          <ul className="list-none mt-4">
+            <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+              <a href="">Connect </a>
+            </li>
+          </ul>
+        </div>
+
+        {/* contact us  */}
+        <div>
+          <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white text-left ">
+            Contact us
+          </h4>
+          <ul className="list-none mt-4">
+            <li className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer `}>
+              <a href="https://mail.google.com/mail/u/0/?fs=1&to=rnxg@sggs.ac.in&tf=cm">rnxg@sggs.ac.in </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
         Copyright Ⓒ 2023 RNXG. All Rights Reserved.
@@ -52,9 +88,8 @@ const Footer = () => (
             key={social.id}
             src={social.icon}
             alt={social.id}
-            className={`w-[21px] h-[21px] object-contain cursor-pointer rounded-full hover:bg-gradient-to-l from-[#00df9a] to-[#DFFA2D] to  ${
-              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-            }`}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer rounded-full hover:bg-gradient-to-l from-[#00df9a] to-[#DFFA2D] to  ${index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+              }`}
             onClick={() => window.open(social.link)}
           />
         ))}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,10 +6,9 @@ import { navLinks } from "../Constant";
 const Navbar = () => {
   const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
-
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <a
+      {/* <a
         className="w-[32px] h-[32px] text-white text-4xl text-bold cursor-pointer"
         href="https://www.rnxg.co.in/"
       >
@@ -29,7 +28,6 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-
       <div className="md:hidden flex flex-1 justify-end items-center">
         <img
           src={toggle ? close : menu}
@@ -54,6 +52,53 @@ const Navbar = () => {
                 <a href={`#${nav.id}`}>{nav.title}</a>
               </li>
             ))}
+          </ul>
+        </div>
+      </div> */}
+      <a
+        className="w-[32px] h-[32px] text-white text-4xl text-bold cursor-pointer"
+        href="https://www.rnxg.co.in/"
+      >
+        RNXG
+      </a>
+      <ul className="list-none md:flex hidden justify-end items-center flex-1 ">
+        <li className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mr-8`}>
+          <a href="/">Home</a>
+        </li>
+        <li className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mr-6 `}>
+          <a href="/treasure">Treasure</a>
+        </li>
+        <li className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mr-4 `}>
+          <a href="/battels">Battles</a>
+        </li>
+        <li className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mr-2`}>
+          <a href="/Contact">Connect</a>
+        </li>
+      </ul>
+      <div className="md:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
+        <div
+          className={`${!toggle ? "hidden" : "flex"
+            } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar z-30 `}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            <li className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mb-1`}>
+              <a href="/">Home</a>
+            </li>
+            <li className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mb-2 `}>
+              <a href="/treasure">Treasure</a>
+            </li>
+            <li className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mb-3 `}>
+              <a href="/battels">Battles</a>
+            </li>
+            <li className={`font-poppins font-normal cursor-pointer text-[16px] text-dimWhite hover:text-white mb-4`}>
+              <a href="/Contact">Connect</a>
+            </li>
           </ul>
         </div>
       </div>
